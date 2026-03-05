50384a8ec0f1ed67b9a3c4c3735dc1db1177da9c
Al omran Abduljaber <alomranabduljaber@gmail.com>
2026-03-05 15:14:01 +0100

    login

--- server/src/auth/controller/authController.ts
+++ server/src/auth/controller/authController.ts
@@ -17,26 +17,32 @@ export default class AuthController {
 
     static async login(req : Request, res : Response) {
         try{
-            if(!req.body){
-                return res.status(404).json()
-            }
-            // Si adresse mail / mot de passe respect la structure imposé par le projet
+          const {email, password } = req.body
+          if(!email || !password){
+                return res.status(400).json({message: "Identifiant requise"})
+          }
+          const [userIfExist] = await UserModel.getUserByEmail(email) as IUser[]
+          if(!userIfExist){
+            return res.status(401).json({message: "Crendential not valide"})
+          }
+          const isValidePassword = await argon2.verify(userIfExist[0] ? userIfExist[0].password : 'null' ,password)
+          if(!isValidePassword){
+            return res.status(401).json({message: "Crendential not valide"})
+          }
+          const generateToken = jwt.sign({user_id: userIfExist[0] && userIfExist[0].id , user_email: userIfExist[0] && userIfExist[0]?.email, role: "user"},process.env.SECRET_KEY || "ejgjdfgg551" , {
+            expiresIn: "30d"
+          } )
+            res
+            .cookie('access_token', `Bearer ${generateToken}`, {
+                expires: new Date(Date.now() + 8 * 3600000) // cookie will be removed after 8 hours
+            })
+          return res.status(200).json({
+            message: "Connexion réussit",
             
-            const [user] = await UserModel.getUserByEmail(req.body.email) as IUser[];
-            if(user && Object.keys(user).length === 0){
-                return res.status(401).json({ message: "Non autorisé" });
-            }
-            if(user){
-                // console.log("user ====>", user);
-                const isValidPassword = await argon2.verify(user.password, req.body.password);
-                if(isValidPassword){
-                    const token = jwt.sign({ user_id : user.id, role: "user"}, process.env.SECRET_KEY || "12345", {});
-                    return res.status(200).json({ message : "pfjvzpo", token });
-                } else {
-                    return res.status(401).json({ message : "Identifiants invalides" })
-                }
-            }
-        } catch {
+          })
+
+        } catch (err){
+            console.log('err :>> ', err);
             res.status(500).json({ message : 'erreur'});
         }
     }
